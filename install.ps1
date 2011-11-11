f5890c1958a5ed8b796246c737ee355422b965aa
José F. Romaniello <jfromaniello@gmail.com>
2011-11-11 10:08:04 -0300

    enhanced install script

--- install.ps1
+++ install.ps1
@@ -1,5 +1,11 @@
 $modulespath = ($env:psmodulepath -split ";")[0]
 $pswatchpath = "$modulespath\pswatch"
-New-Item -Type Container -Force -path $pswatchpath
 
+Write-Host "Creating module directory"
+New-Item -Type Container -Force -path $pswatchpath | out-null
+
+Write-Host "Downloading and installing"
 (new-object net.webclient).DownloadString("https://raw.github.com/jfromaniello/pswatch/master/pswatch.psm1") | Out-File "$pswatchpath\pswatch.psm1" 
+
+Write-Host "Installed!"
+Write-Host 'Use "Import-Module pswatch" and then "watch"'
