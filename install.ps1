b13a45d34eeceed8e6737790d7e0776e6e777ea3
José F. Romaniello <jfromaniello@gmail.com>
2011-11-11 10:03:13 -0300

    fixed install script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
 $modulespath = ($env:psmodulepath -split ";")[0]
-$pswatchpath = "$modulespath\pswatch\"
+$pswatchpath = "$modulespath\pswatch"
 New-Item -Type Container -Force -path $pswatchpath
+
+(new-object net.webclient).DownloadString("https://raw.github.com/jfromaniello/pswatch/master/pswatch.psm1") | Out-File "$pswatchpath\pswatch.psm1" 
